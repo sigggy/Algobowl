f6b0c2514a5fc8aaba9a9055f2bee645c52afdda
sigggy <jackbsigler@gmail.com>
2024-10-03 21:06:53 -0600

    greedy is almost done

--- graphCreation.py
+++ graphCreation.py
@@ -1,10 +1,12 @@
+# flake8: noqa
 import numpy as np
 from output_verifier import *
 from bowl import write_output, light_bulbs
 import copy
 from test import *
+import random
 
-def find_important_sqaures(board, lightmap, nummap):
+def find_important_squares(board, lightmap, nummap):
     retMap = copy.deepcopy(board)
     
     directions = {
@@ -19,15 +21,16 @@ def find_important_sqaures(board, lightmap, nummap):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] in important_nums:
-                retMap[i][j] = number_tile(i, j, board[i][j])
+                retMap[i][j] = number_tile(j, i, board[i][j])
                 nummap[i][j] = retMap[i][j]
                 for direction in directions.values():
                     dr, dc = direction
                     r, c = i + dr, j + dc
                     # Check only one tile over
                     if 0 <= r < len(board) and 0 <= c < len(board[0]):
-                        retMap[r][c] = '!'
-                        lightmap[r][c] = number_tile_light(True, r, c)
+                        if board[r][c] == '.':
+                            retMap[r][c] = '!'
+                            lightmap[r][c] = number_tile_light(True, c, r)
     return retMap
 
 def find_neighbors_light(retMap, lightmap, i, j):
@@ -57,6 +60,7 @@ def find_neighbors_light(retMap, lightmap, i, j):
             break
         if lightmap[y][j]:
             lightmap[i][j].neighbors.append(lightmap[y][j])
+    lightmap[i][j].collisions = len(lightmap[i][j].neighbors)
 
 def print_grid(grid: List[List[str]]) -> None:
     for row in grid:
@@ -88,6 +92,96 @@ def find_collisions(board, retMap, nummap, lightmap):
                 find_neigh_num(nummap, lightmap, i, j)
             if retMap[i][j] == "!":
                 find_neighbors_light(board, lightmap, i, j)
+                
+
+def printer(lightmap, nummap, retMap, map):
+    for i in range(len(lightmap)):
+        for j in range(len(lightmap[0])):
+            if lightmap[i][j]:
+                print(lightmap[i][j].__str__(), end= ' ')
+                continue 
+            print(lightmap[i][j], end = ' ')
+        print()
+    print()
+
+    for i in range(len(lightmap)):
+        for j in range(len(lightmap[0])):
+            if lightmap[i][j]:
+                print(f'light map at {lightmap[i][j].__str__()} with {lightmap[i][j].collisions} collisions')
+                for element in lightmap[i][j].neighbors:
+                    print(f' -collision at {element.__str__()}')
+    
+                print()
+        print()
+        
+    for i in range(len(nummap)):
+        for j in range(len(nummap[0])):
+            if nummap[i][j]:
+                print(nummap[i][j].__str__(), end= ' ')
+                continue 
+            print(nummap[i][j], end = ' ')
+        print()
+    print()
+
+    for i in range(len(nummap)):
+        for j in range(len(nummap[0])):
+            if nummap[i][j]:
+                print(f'light map at {nummap[i][j].__str__()}')
+                for element in nummap[i][j].adjacent_lights:
+                    print(f' -adajcent at {element.__str__()}')
+    
+                print()
+        print()
+
+    for i in range (len(retMap)):
+        for j in range(len(retMap[0])):
+            if retMap[i][j]:
+                print(retMap[i][j].__str__(), end= ' ')
+                continue 
+            print(retMap[i][j], end = ' ')
+        print()
+    print()
+
+    for i in range (len(map)):
+        for j in range(len(map[0])):
+            if map[i][j]:
+                print(map[i][j].__str__(), end= ' ')
+                continue 
+            print(map[i][j], end = ' ')
+        print()
+    print()
+
+def get_nums(nummap):
+    nums_list = list()
+    for i in range(len(nummap)):
+        for j in range(len(nummap[0])):
+            if nummap[i][j]:
+                nums_list.append(nummap[i][j])
+
+    # Return a new shuffled version of nums_list
+    return random.sample(nums_list, len(nums_list))
+
+def update_map(lightMap, map):
+    for i in range (len(map)):
+        for j in range(len(map[0])):
+            if lightMap[i][j] and not lightMap[i][j].is_lit:
+                map[i][j] = '.'
+                continue 
+          
+
+
+def eliminate_collisions(nummap):
+    nums_list = get_nums(nummap)
+
+    for number in nums_list:
+        number.pop_light()
+
+    for number in nums_list:
+        number.pop_light()
+    
+
+
+
 
 
 
@@ -96,16 +190,20 @@ def main():
     _, retMap = get_input_data(sys.argv[1])
     lightmap = [[None for _ in range(len(retMap[0]))] for _ in range(len(retMap))]
     nummap = [[None for _ in range(len(retMap[0]))] for _ in range(len(retMap))]
-    map = find_important_sqaures(retMap, lightmap, nummap)
+    map = find_important_squares(retMap, lightmap, nummap)
     find_collisions(retMap, map, nummap, lightmap)
+    printer(lightmap, nummap, retMap, map)
+    eliminate_collisions(nummap)
+    update_map(lightmap, map)
+    printer(lightmap, nummap, retMap, map)
 
 
-    for i in range(len(lightmap)):
-        for j in range(len(lightmap[0])):
-            if lightmap[i][j]:
-                lightmap[i][j] = lightmap[i][j].__str__()
-            print(lightmap[i][j], end = ' ')
-        print()
+
+
+
+
+    
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -6,16 +6,41 @@ import random
 
 class number_tile:
     def __init__(self,x_pos, y_pos, num):
-        self.num = num
+        self.num = int(num)
         self.i = x_pos
         self.j = y_pos
         self.adjacent_lights = []
 
     def pop_light(self, ):
-        pass
+        if len(self.adjacent_lights) == self.num:
+            return 
+
+
+        if len(self.adjacent_lights) < self.num:
+            print(f'num {self} cannot be satisfied')
+            for light in self.adjacent_lights:
+                print(f"Removed light: {light}")
+                light.rebalance_light()
+            self.adjacent_lights = []
+            
+            return
+        while len(self.adjacent_lights) > self.num:
+            
+            max_collisions = max(self.adjacent_lights, key=lambda light: light.collisions).collisions
+            max_lights = [light for light in self.adjacent_lights if light.collisions == max_collisions]
+            light_to_remove = random.choice(max_lights)
+
+            
+            light_to_remove.rebalance_light()
+            self.adjacent_lights.remove(light_to_remove)
+            print(f"Removed light: {light_to_remove}")
+
+                
     
     def __str__(self) -> str:
+        return f'{self.num} ({self.i},{self.j})'
         return f'{self.num}'
+
 
 class number_tile_light:
     def __init__(self, is_lit, i, j):
@@ -25,8 +50,26 @@ class number_tile_light:
         self.neighbors = []
         self.collisions = 0
 
-    def rebalance_light(self, ): 
-        pass
+        # TODO need to make a pointer to the number a light is adjcant to so I can remove teh light from it 
+        # TODO If you want a number_tile_light to be able to remove itself from multiple number_tile objects that it is adjacent to, you need to create a way to track the relationship between each number_tile_light and all the number_tile objects it is adjacent to.
+    def rebalance_light(self,): 
+        self.is_lit = False 
+        for collision in self.neighbors:
+            if self in collision.neighbors:
+                print(len(collision.neighbors))
+                collision.neighbors.remove(self)
+                print(f"Removed self ({self.i}, {self.j}) from neighbor ({collision.i}, {collision.j})")
+               
+                collision.collisions = len(collision.neighbors)
+                print(len(collision.neighbors))
+                print()
+        self.neighbors = []
+        print(f"Rebalanced light at ({self.i}, {self.j}) and removed all neighbors.")
+        print()
+        print()
+
+
+
     
     def __str__(self) -> str:
         return f"({self.i}, {self.j})"
